8e86f70f3387c3bcf6d4205bde91ddd1465c80a9
BLueWater419 <g0ldcrafter3.0@gmail.com>
2025-03-25 12:32:54 -0500

    sfml update 3

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <deque>
 #include <set>
+#include <map>
 #include <SFML/Graphics.hpp>
 
 std::vector<std::pair<int, int>> get_important_indices(std::vector<std::string> map, int line_length) {
@@ -20,7 +21,7 @@ std::vector<std::pair<int, int>> get_important_indices(std::vector<std::string> 
     return indices;
 }
 
-std::pair<int, int> get_starting_position(std::vector<std::string> map, int line_length) {
+std::pair<int, int> get_player_position(std::vector<std::string> map, int line_length) {
     std::pair<int, int> pair;
 
     for (int row_index = 0; row_index < map.size(); row_index++) {
@@ -109,7 +110,7 @@ bool map_has_correct_paths(const std::vector<std::string>& map, std::pair<int, i
     std::cout << "Getting indices.\n";
     std::vector<std::pair<int, int>> important_indices = get_important_indices(map, hw_pair.second);
     std::cout << "Getting starting position.\n";
-    std::pair<int, int> starting_pos = get_starting_position(map, hw_pair.second);
+    std::pair<int, int> starting_pos = get_player_position(map, hw_pair.second);
 
     for (std::pair<int, int> target : important_indices) {
         std::set<std::pair<int, int>> visited;
@@ -143,8 +144,19 @@ bool is_map_valid(std::vector<std::string> map) {
     return map_has_correct_paths(map, hw_pair);
 }
 
+
+void display_full_map(std::vector<std::string> map, int height, int width) {
+    for (int row = 0; row < height; ++row) {
+        for (int col = 0; col < width; ++col) {
+            std::cout << map[row][col];
+        }
+
+        std::cout << "\n";
+    }
+}
+
 int main() {
-    std::string file_directory = "C:/Users/g0ldc/OneDrive/Dokumente/Projects/CPP/game/maps/map1.txt";
+    std::string file_directory = ".\\maps\\map1.txt";
 
     std::ifstream file(file_directory);
     std::string my_text;
@@ -168,70 +180,176 @@ int main() {
 
     file.close();
     if (!is_valid) {
-        std::cout << "WRONG!11!1";
+        std::cout << "WRONG!11!1" << "\n";
         // return -1;
     } else {
-        std::cout << "VALID!!1!1";
+        std::cout << "VALID!!1!1" << "\n";
     }
+
+    int turns = 1000;
+    int points = 0;
     
     double window_size_x = 1920;
     double window_size_y = 1080;
 
-    sf::Texture wall_texture(".\\textures\\wall.png");
-    sf::Texture free_space_texture(".\\textures\\free_space.png");
-    sf::Texture player_texture(".\\textures\\player.png");
-    sf::Texture exit_texture(".\\textures\\exit.png");
-    sf::Texture collectible_texture(".\\textures\\collectible.png");
+    sf::Font Tektur;
+    Tektur.openFromFile(".\\fonts\\Tektur\\static\\Tektur-Regular.ttf");
+
+    sf::Text turn_count_instance(Tektur);
+    turn_count_instance.setString("Turns:\n" + std::to_string(turns));
+    turn_count_instance.setPosition(sf::Vector2f(0, 0));
+
+    sf::Text point_count_instance(Tektur);
+    point_count_instance.setString("Points:\n" + std::to_string(points));
+    point_count_instance.setPosition(sf::Vector2f(0, 100));
+
+    sf::Texture wall_texture;
+    wall_texture.loadFromFile(".\\textures\\wall.png");
+
+    sf::Texture free_space_texture;
+    free_space_texture.loadFromFile(".\\textures\\free_space.png");
+
+    sf::Texture player_texture;
+    player_texture.loadFromFile(".\\textures\\player.png");
+
+    sf::Texture exit_texture;
+    exit_texture.loadFromFile(".\\textures\\exit.png");
+
+    sf::Texture collectible_texture;
+    collectible_texture.loadFromFile(".\\textures\\collectible.png");
 
     sf::RenderWindow window(sf::VideoMode({window_size_x, window_size_y}), "SFML");
-    window.setFramerateLimit(60);
+    window.setFramerateLimit(10);
 
     double square_size = std::min(window_size_x / width, window_size_y / height);
 
     double offset_x = (window_size_x - (width * square_size)) / 2.0;
     double offset_y = (window_size_y - (height * square_size)) / 2.0;
 
-    std::vector<sf::Sprite> sprites;
+    std::map<std::pair<int, int>, sf::Sprite> sprites;
+
+    sf::Sprite player_sprite(player_texture);
+    player_sprite.setScale(sf::Vector2f(square_size, square_size));
 
     for (int row = 0; row < height; ++row) {
         for (int col = 0; col < width; ++col) {
             sf::Sprite sprite(free_space_texture);
-            
+
 
             if (map[row][col] == '1') {
                 sprite.setTexture(wall_texture);
             } else if (map[row][col] == '0') {
                 sprite.setTexture(free_space_texture);
             } else if (map[row][col] == 'P') {
-                sprite.setTexture(player_texture);
+                player_sprite.setPosition(sf::Vector2f(col * square_size + offset_x, row * square_size + offset_y));
             } else if (map[row][col] == 'E') {
                 sprite.setTexture(exit_texture);
             } else if (map[row][col] == 'C') {
                 sprite.setTexture(collectible_texture);
             }
 
-            sprite.setScale(sf::Vector2f(square_size, square_size));
-            sprite.setPosition(sf::Vector2f(col * square_size, row * square_size));
+            sf::Vector2u texture_size = sprite.getTexture().getSize();
 
-            sprites.push_back(sprite);
+            float scale_x = square_size / static_cast<float>(texture_size.x);
+            float scale_y = square_size / static_cast<float>(texture_size.y);
+
+            float scale_factor = std::min(scale_x, scale_y);
+            sprite.setScale(sf::Vector2f(scale_factor, scale_factor));
+
+            sf::Vector2f position(col * square_size + offset_x, row * square_size + offset_y);
+            sprite.setPosition(sf::Vector2f(position.x + (square_size - texture_size.x * scale_factor) / 2.0f,
+            position.y + (square_size - texture_size.y * scale_factor) / 2.0f));
+
+            sprites.insert({std::make_pair(row, col), sprite});
         }
     }
+
+    sf::Vector2u player_texture_size = player_texture.getSize();
+    float player_scale_x = square_size / static_cast<float>(player_texture_size.x);
+    float player_scale_y = square_size / static_cast<float>(player_texture_size.y);
+    float player_scale_factor = std::min(player_scale_x, player_scale_y);
+    player_sprite.setScale(sf::Vector2f(player_scale_factor, player_scale_factor));
+
+    sf::Vector2f player_position = player_sprite.getPosition();
+    player_sprite.setPosition(sf::Vector2f(player_position.x + (square_size - player_texture_size.x * player_scale_factor) / 2.0f,
+    player_position.y + (square_size - player_texture_size.y * player_scale_factor) / 2.0f));
 
     while (window.isOpen()) {
         while (const std::optional event = window.pollEvent()) {
             if (event->is<sf::Event::Closed>())
                 window.close();
 
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Escape)) window.close();
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Escape))
+                window.close();
+        }
+
+        std::pair<int, int> current_direction = {0, 0};
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
+            current_direction.second -= 1;
+        } 
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
+            current_direction.second += 1;
+        } 
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {
+            current_direction.first += 1;
+        } 
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
+            current_direction.first -= 1;
+        }
+
+        std::pair<int, int> current_pos = get_player_position(map, width);
+
+        char last_cell = '0';
+        bool reached_exit = false;
+
+        int next_x = current_pos.second + current_direction.first;
+        int next_y = current_pos.first + current_direction.second;
+
+        if (next_x >= 0 && next_x < width && next_y >= 0 && next_y < height && map[next_y][next_x] != '1') {
+            map[current_pos.first][current_pos.second] = last_cell;
+            last_cell = map[next_y][next_x];
+            
+            if (last_cell == 'C') {
+                map[next_y][next_x] = '0';
+                sprites.at(std::pair(next_y, next_x)).setTexture(free_space_texture);
+
+                points += 1;
+                point_count_instance.setString("Points:\n" + std::to_string(points));
+            } else if (last_cell == 'E') {
+                map[next_y][next_x] = '0';
+                sprites.at(std::pair(next_y, next_x)).setTexture(free_space_texture);
+                reached_exit = true;
+            }
+
+            map[next_y][next_x] = 'P';
+
+            sf::Vector2f position(next_x * square_size + offset_x, next_y * square_size + offset_y);
+            player_sprite.setPosition(position);
+
+            if (current_direction != std::make_pair(0, 0)) {
+                turns -= 1;
+                turn_count_instance.setString("Turns:\n" + std::to_string(turns));
+            }
         }
 
         window.clear();
 
-        for (sf::Sprite sprite : sprites) {
-            window.draw(sprite);
+        for (auto sprite : sprites) {
+            window.draw(sprite.second);
         }
 
+        window.draw(player_sprite);
+        window.draw(turn_count_instance);
+        window.draw(point_count_instance);
+
         window.display();
+
+        //display_full_map(map, height, width);
+
+        if (reached_exit || turns <= 0) {
+            window.close();
+        }
     }
 
     return 0;
