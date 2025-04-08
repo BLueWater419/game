5a747141b11292735072db34d8d81e48f90eeec2
BLueWater419 <g0ldcrafter3.0@gmail.com>
2025-04-08 13:01:25 -0500

    cleaned up main.cpp

--- code/main.cpp
+++ code/main.cpp
@@ -41,71 +41,11 @@ int main() {
     file.close();
     if (!is_valid) {
         std::cout << "WRONG!11!1" << "\n";
-        // return -1;
+        return -1;
     } else {
         std::cout << "VALID!!1!1" << "\n";
     }
 
     render(map, width, height);
-
     return 0;
 }
-
-/*
-bool map_has_correct_paths_loop(std::vector<std::string> map, std::pair<int, int> hw_pair) {
-    std::vector<std::pair<int, int>> directions = {
-        std::make_pair(0, 1), std::make_pair(1, 0),
-        std::make_pair(0, -1), std::make_pair(-1, 0)
-    };
-
-    std::vector<std::pair<int, int>> important_indices = get_important_indices(map, hw_pair.second);
-    std::pair<int, int> starting_pos = get_starting_position(map, hw_pair.second);
-
-    for (std::pair<int, int> indices : important_indices) {
-        std::deque<std::pair<int, int>> queue;
-        std::vector<std::pair<int, int>> explored;
-
-        bool found = false;
-
-        queue.push_back(starting_pos);
-        explored.push_back(starting_pos);
-
-        while (!queue.empty()) {
-            std::pair<int, int> v = queue.front();
-            queue.pop_front();
-
-            for (std::pair<int, int> pair : directions) {
-                int next_x = (v.second + pair.second) % hw_pair.second;
-                int next_y = (v.first + pair.first) % hw_pair.first;
-                std::pair<int, int> next_pair = std::make_pair(next_y, next_x);
-
-                if (is_pair_in_vector(next_pair, explored)) {
-                    continue;
-                }
-                
-                char next_cell = map.at(next_y)[next_x];
-                if (next_cell == '0' || next_cell == 'C' || next_cell == 'E') {
-                    if (next_x == indices.second && next_y == indices.first) {
-                        found = true;
-                        break;
-                    } else {
-                        explored.push_back(next_pair);
-                        queue.push_back(next_pair);
-                    }
-                }
-            }
-
-            if (found) {
-                break;
-            }
-        }
-
-        if (!found) {
-            return false;
-        }
-    }
-
-    return true;
-}
-
-*/
